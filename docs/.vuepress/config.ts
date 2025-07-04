163f437b75f2109095b1717cc869ca47bf871725
kazumatu981 <kazumatu981@gmail.com>
2025-07-05 08:21:14 +0900

    まずは書き始めの下地作り

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,43 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite';
+import { defaultTheme } from '@vuepress/theme-default';
+import { defineUserConfig } from 'vuepress';
 
 export default defineUserConfig({
-  bundler: viteBundler(),
-  theme: defaultTheme(),
+    lang: 'ja-JP',
+    base: '/shikake/',
+    title: 'しかけ',
+    bundler: viteBundler(),
+    theme: defaultTheme({
+        sidebar: [
+            {
+                text: 'home',
+                link: '/',
+            },
+            {
+                text: '世の中の仕掛けを図に表現しよう',
+                link: '/getting-started/',
+            },
+            {
+                text: 'どうやって書くの',
+                children: [
+                    {
+                        text: 'どう使われるかを書く',
+                        link: '/usecase/',
+                    },
+                    {
+                        text: 'システムのカタチを書く',
+                        link: '/static/',
+                    },
+                    {
+                        text: 'システムの動きを書く',
+                        link: '/dynamic/',
+                    },
+                ],
+            },
+            {
+                text: 'サンプル',
+                link: '/samples/',
+            },
+        ],
+    }),
 });
